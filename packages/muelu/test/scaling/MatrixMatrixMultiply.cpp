a7802cf9c56c02295a3129759465a057a9e74b2a
Brian Kelley <bmkelle@sandia.gov>
2019-09-25 16:38:25 -0700

    Initial Tpetra stack nightly performance tests
    Tpetra:        Added CGSolve strong scaling (MPI procs = 1, 4, 9, ...) tests
    Kokkos/Zoltan: Fixed test categories, test exectuable names for perf tests
    MueLu:         Perf tests for driver (Laplace3D setup+solve) and mat-mat

--- packages/muelu/test/scaling/MatrixMatrixMultiply.cpp
+++ packages/muelu/test/scaling/MatrixMatrixMultiply.cpp
@@ -192,7 +192,6 @@ int main_(Teuchos::CommandLineProcessor &clp, Xpetra::UnderlyingLib &lib, int ar
 	//  std::string fileName="checkAB.mm";
 	//  Utils::Write( fileName,*AB);
 	//}
-	if (comm->getRank() == 0) std::cout << "success" << std::endl;
 	
       } //scope for multiply
 
@@ -206,8 +205,10 @@ int main_(Teuchos::CommandLineProcessor &clp, Xpetra::UnderlyingLib &lib, int ar
       TimeMonitor::summarize();
     }
 
-    bool success = true;
-    return ( success ? EXIT_SUCCESS : EXIT_FAILURE );
+    if (comm->getRank() == 0)
+      std::cout << "End Result: TEST PASSED";
+
+    return 0;
 }
  //main_
 
